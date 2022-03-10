0a18e2d3ac2d0793e6b2615513c364e273e62225
Mauro de Oliveira Andre <mauroolivere@hotmail.com>
2022-03-10 14:22:00 +0100

    Initial Commit

---
+++ Zoe-Vida De Deus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zoe-Vida De Deus
+//
+//  Created by Mauro Sys One on 3/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zoe-Vida De Deus/Zoe_Vida_De_DeusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zoe_Vida_De_DeusApp.swift
+//  Zoe-Vida De Deus
+//
+//  Created by Mauro Sys One on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Zoe_Vida_De_DeusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
